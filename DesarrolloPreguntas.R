53901f5520829574612b8c2f2c922190dc807af2
eddmenz97 <mendez.curiante.edward@gmail.com>
2019-09-27 20:46:52 -0300

    avance en el trabajo

---
+++ DesarrolloPreguntas.R
@@ -0,0 +1,38 @@
+
+# LISTA DE NUMEROS
+listaDeNumeros <- list(2,5,6,2,1,5,6,10,11,20,15)
+listaDeNumeros[5]
+listaDeNumeros[0]
+
+listaDeNumeros[0]
+listaDeNumeros[13]
+listaDeNumeros[11]
+
+#Ejercicio 2
+unlist(listaDeNumeros[5])
+#no varia"
+
+listaDeNumeros[5] <- 12
+if(listaDeNumeros[5]+1>0){
+  print("se cumple")
+}
+if(unlist(listaDeNumeros[5])+1>0){
+  print("se cumple")
+}
+
+
+listaDeNumeros[5]<-12
+
+length(nombre_variable)
+length(listaDeNumeros)
+
+"ejercicio 5"
+
+valorinicial<-5
+valorfinal<-20
+valorinicial<-11
+valorfinal<-5
+
+valorinicial:length(listaDeNumeros)
+length(listaDeNumeros[5] <- 12)
+
